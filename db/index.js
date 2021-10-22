fbdf932dec272753c3d8034c56f1dd5347a13d13
k2pbac <krisbachan123@hotmail.com>
2021-10-22 15:42:39 -0400

    remove env file from root of folder

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const pg = require("pg");
-
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 
 const client = new pg.Client({
--- db/reset.js
+++ db/reset.js
@@ -15,10 +15,10 @@ const client = new Client();
 // Loads the schema files from db/schema
 const runSchemaFiles = function () {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
-  const schemaFilenames = fs.readdirSync("./db/schema");
+  const schemaFilenames = fs.readdirSync("./schema");
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/schema/${fn}`, "utf8");
+    const sql = fs.readFileSync(`./schema/${fn}`, "utf8");
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
@@ -26,10 +26,10 @@ const runSchemaFiles = function () {
 
 const runSeedFiles = function () {
   console.log(chalk.cyan(`-> Loading Seeds ...`));
-  const schemaFilenames = fs.readdirSync("./db/seeds");
+  const schemaFilenames = fs.readdirSync("./seeds");
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/seeds/${fn}`, "utf8");
+    const sql = fs.readFileSync(`./seeds/${fn}`, "utf8");
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
