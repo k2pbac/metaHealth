5ac66a147e89de183c63507354412d32328a61ce
k2pbac <krisbachan123@hotmail.com>
2021-10-24 17:27:16 -0400

    split header content into header and main

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -17,12 +17,6 @@ const Header = (props) => {
             since 2021.
           </p>
         </div>
-        <div className="middle">
-          <img src="https://images.unsplash.com/photo-1558403194-611308249627?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"></img>
-          <section>
-            <h1>Welcome to Meta Health</h1>
-          </section>
-        </div>
       </header>
     </Container>
   );
--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,9 @@ import Carousel from "react-bootstrap/Carousel";
 import "index.scss";
 
 import Home from "components/Home/Home";
+import Body from "components/Home/Body";
 import Header from "components/Home/Header";
 
-storiesOf("Home", module).add("Header", () => <Header></Header>);
+storiesOf("Home", module)
+  .add("Header", () => <Header></Header>)
+  .add("Body", () => <Body></Body>);
