f9ec9be60f672bf1f05ecb6c4fefd0869462d157
k2pbac <krisbachan123@hotmail.com>
2021-10-25 11:22:52 -0400

    update ui for body with Row's and padding/margin

--- src/components/Home/Body.jsx
+++ src/components/Home/Body.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "components/Home/Body.scss";
 import Button from "react-bootstrap/Button";
 import TestimonialList from "./TestimonialList";
-
+import Row from "react-bootstrap/Row";
+import Column from "react-bootstrap/Col";
 const EmployeeData = [
   {
     id: 1,
@@ -63,25 +64,30 @@ const PatientData = [
 const Body = (props) => {
   return (
     <main>
-      <div className="middle">
-        <img src="https://images.unsplash.com/photo-1558403194-611308249627?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"></img>
-        <section>
-          <h1>Welcome to Meta Health</h1>
-          <p>
-            We are a profressional organization, working day and night to make
-            getting in touch with a health care provider easier. Whether you are
-            looking for a family doctor, a walk-in clinic, a physiotherapist, or
-            anything in between, we will help you.
-          </p>
-          <Button
-            variant="outline-dark"
-            size="lg"
-            className="blue-outline rounded-0"
-          >
-            Get More Info
-          </Button>
-        </section>
-      </div>
+      <Row className="more-info">
+        <div className="middle">
+          <img src="https://images.unsplash.com/photo-1558403194-611308249627?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"></img>
+          <section>
+            <h1>Welcome to Meta Health</h1>
+            <p>
+              We are a profressional organization, working day and night to make
+              getting in touch with a health care provider easier. Whether you
+              are looking for a family doctor, a walk-in clinic, a
+              physiotherapist, or anything in between, we will help you.
+            </p>
+            <Button
+              variant="outline-dark"
+              size="lg"
+              className="blue-outline rounded-0"
+            >
+              Get More Info
+            </Button>
+          </section>
+        </div>
+      </Row>
+      <Row>
+        <TestimonialList peopleData={PatientData}></TestimonialList>
+      </Row>
     </main>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import Body from "./Body";
+import Button from "components/Button";
 
 const Home = (props) => {
   return (
     <>
+      <Button>Login</Button>
       <Header></Header>
+      <Body></Body>
     </>
   );
 };
