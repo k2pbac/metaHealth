ab76a2d6deedd6200ccd4e75bf37cf99d2c83be1
k2pbac <krisbachan123@hotmail.com>
2021-10-25 17:58:40 -0400

    update footer ui and change to relevant links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,55 +1,53 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
 import "./Footer.scss";
-
+import ListGroup from "react-bootstrap/ListGroup";
 const Footer = (props) => {
   return (
     <footer className="page-footer pt-4">
       <div className="container-fluid text-center text-md-left">
-        <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <h5 className="text-uppercase">Footer Content</h5>
-            <p>
-              Here you can use rows and columns to organize your footer content.
-            </p>
+        <div className="row justify-content-around">
+          <div className="col-md-6 mt-md-0 mt-5">
+            <div className="d-flex flex-column p-0">
+              <p>
+                <span className="fs-5">M</span>eta Health, connecting patients
+                with health care professionals since 2021.
+              </p>
+              <label for="basic-url">Follow us</label>
+              <ListGroup className="justify-content-center" horizontal>
+                <Image
+                  className="p-2"
+                  src="https://img.icons8.com/color/36/000000/twitter--v1.png"
+                />
+                <Image
+                  className="p-2"
+                  src="https://img.icons8.com/nolan/36/instagram-new.png"
+                ></Image>
+                <Image
+                  className="p-2"
+                  src="https://img.icons8.com/color/36/000000/facebook.png"
+                ></Image>
+                <Image src="https://img.icons8.com/plasticine/48/000000/snapchat.png" />
+              </ListGroup>
+            </div>
           </div>
           <hr className="clearfix w-100 d-md-none pb-3" />
           <div className="col-md-3 mb-md-0 mb-3">
-            <h5 className="text-uppercase">Links</h5>
-
             <ul className="list-unstyled">
               <li>
-                <a href="#!">Link 1</a>
+                <a href="#!">Terms and Policies</a>
               </li>
               <li>
-                <a href="#!">Link 2</a>
+                <a href="#!">Contact Us</a>
               </li>
               <li>
-                <a href="#!">Link 3</a>
+                <a href="#!">Employee Services</a>
               </li>
               <li>
-                <a href="#!">Link 4</a>
+                <a href="#!">Patient Services</a>
               </li>
             </ul>
           </div>{" "}
-          <div className="col-md-3 mb-md-0 mb-3">
-            <h5 className="text-uppercase">Links</h5>
-
-            <ul className="list-unstyled">
-              <li>
-                <a href="#!">Link 1</a>
-              </li>
-              <li>
-                <a href="#!">Link 2</a>
-              </li>
-              <li>
-                <a href="#!">Link 3</a>
-              </li>
-              <li>
-                <a href="#!">Link 4</a>
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
 
