fddaed5a8cc936bcc2b83d6cc2fce808876f1b87
k2pbac <krisbachan123@hotmail.com>
2021-10-25 09:24:51 -0400

    update testimonial ui, add section for employee or patient

--- src/components/Home/Testimonial.jsx
+++ src/components/Home/Testimonial.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import Image from "react-bootstrap/Image";
 import "./Testimonial.scss";
 import Card from "react-bootstrap/Card";
-const Testimonial = ({ companyName, description, image, name, position }) => {
+const Testimonial = ({
+  companyName,
+  description,
+  image,
+  name,
+  position,
+  role,
+}) => {
   return (
     <Card className="shadow-sm" style={{ width: "18rem" }}>
       <Card.Body>
-        <Card.Title className="text-muted">{companyName}</Card.Title>
+        <Card.Title className="text-muted">
+          {companyName}{" "}
+          <Card.Subtitle className="mb-2 text-muted d-inline">
+            ({role})
+          </Card.Subtitle>
+        </Card.Title>
         <Image
           className="quotes"
           alt="quotation marks"
           src="images/quotes.png"
         ></Image>
+
         <Card.Text>{description}</Card.Text>
       </Card.Body>
       <div className="d-flex align-items-end">
--- stories/index.js
+++ stories/index.js
@@ -11,13 +11,14 @@ import Testimonial from "components/Home/Testimonial";
 storiesOf("Home", module)
   .add("Header", () => <Header></Header>)
   .add("Body", () => <Body></Body>)
-  .add("Employee-Testimonials", () => (
+  .add("Employee-Testimonial", () => (
     <Testimonial
       companyName="Sabertooth"
       description="Wow! This application is amazing. It has made my life working at Sabertooth much easier! Thank you all at Meta Health!"
       image="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1061&q=80"
       name="JANICE KULAS"
       position="Assistant Manager"
+      role="Employee"
     ></Testimonial>
   ))
   .add("Patient-Testimonials", () => <Testimonial></Testimonial>);
