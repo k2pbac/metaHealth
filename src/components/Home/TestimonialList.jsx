2d8b86468dc7a8b24a8ae19b0255725188d4b3d5
k2pbac <krisbachan123@hotmail.com>
2021-10-25 22:01:38 -0400

    remove carousel and add animated arrows to work similar to carousel

--- src/components/Home/TestimonialList.jsx
+++ src/components/Home/TestimonialList.jsx
@@ -3,6 +3,11 @@ import React, { useState } from "react";
 import Testimonial from "./Testimonial";
 import "./Testimonial.scss";
 import "./TestimonialList.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+library.add(faArrowRight, faArrowLeft);
+
 const TestimonialList = ({ patientData, employeeData }) => {
   const [currentView, setCurrentView] = useState(false);
   const patients =
@@ -40,9 +45,15 @@ const TestimonialList = ({ patientData, employeeData }) => {
   return (
     <div className="d-flex flex-column align-items-center">
       <h1 className="text-center mb-4">{patientData[0].role} Testimonials</h1>
-      <div className="d-flex">
+      <div className="d-flex list-container">
         {currentView && patients}
         {!currentView && employees}
+        <div class="arrow-right">
+          <FontAwesomeIcon size="2x" icon={faArrowRight} />
+        </div>
+        <div class="arrow-left">
+          <FontAwesomeIcon size="2x" icon={faArrowLeft} />
+        </div>
       </div>
     </div>
   );
