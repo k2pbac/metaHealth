24b97d15f22dd697e0f2ef5e6860349c29e19667
k2pbac <krisbachan123@hotmail.com>
2021-10-26 22:15:13 -0400

    complete login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,28 +2,31 @@ import React from "react";
 import "./LoginForm.scss";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 const LoginForm = (props) => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container className="login-form bg-light w-50 h-100 text-center shadow-sm">
+      <h1 className="mb-4">Login</h1>
+      <Form className="w-50 mx-auto">
+        <Form.Group className="mb-3" controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <div className="d-flex justify-content-center">
+          <Button className="register-link" type="submit">
+            Sign Up
+          </Button>
+          <Button className="login-link" type="button">
+            Login
+          </Button>
+        </div>
+      </Form>
+    </Container>
   );
 };
 
