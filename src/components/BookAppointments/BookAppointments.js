0de728af2faf10533021288e7eda85ca3a130814
Faadhil <faadhil.aa@gmail.com>
2021-10-28 11:06:46 -0400

    create clinic card component

---
+++ src/components/BookAppointments/BookAppointments.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import classNames from "classnames"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Image from 'react-bootstrap/Image';
+import Button from "react-bootstrap/Button";
+
+import "components/BookAppointments/BookAppointments.scss"
+
+export default function BookAppointments(props){
+
+const {
+name,
+address,
+} = props;
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/BookAppointments/ClinicCard.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import classNames from "classnames"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Image from 'react-bootstrap/Image';
+import Button from "react-bootstrap/Button";
+
+import "components/BookAppointments/BookAppointments.scss"
+
+export default function ClinicCard(props) {
+
+  const {
+    name,
+    address,
+    image
+  } = props;
+
+  return (
+    <div className="clinic-card">
+      <div>
+        <Image className="clinic-image" src={image} alt={name}></Image>
+      </div>
+      <div className="clinic-name">{name}</div>
+      <div className="clinic-address">{address}</div>
+    </div>
+  )
+}
