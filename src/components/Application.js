05ec1a5cd82780387491ec072751adc6a5f17724
k2pbac <krisbachan123@hotmail.com>
2021-10-28 18:13:50 -0400

    reinstall react router

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Route, Switch } from "react-router-dom";
 import "components/Application.scss";
 import Home from "./Home/Home";
 import Footer from "./Footer";
 import LoggedInPatient from "./Navbar/LoggedState/LoggedInPatient";
-
+import Navbar from "./Navbar/NavHeader";
+import LoginSelectionPanel from "./Register_and_Login_Selection/LoginSelectionPanel";
 export default function Application(props) {
   return (
     <>
-      <LoggedInPatient></LoggedInPatient>
-      <Home></Home>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={LoginSelectionPanel} />
+      </Switch>
       <Footer></Footer>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "index.scss";
 
 import Application from "components/Application";
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Application />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
