d5158090f833395561982570402c2b59b619f038
k2pbac <krisbachan123@hotmail.com>
2021-10-28 08:35:05 -0400

    add patientData to component and display patient/clinic details

--- src/components/ManageAppointments/PatientDetails.jsx
+++ src/components/ManageAppointments/PatientDetails.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 import "./PatientDetails.scss";
-
-const PatientDetails = (props) => {
-  return <div></div>;
+import { patientReport } from "./PatientReport";
+const PatientDetails = ({ patient }) => {
+  return (
+    <div className="border p-4 w-25 d-flex flex-column">
+      <div className="mb-5">
+        <h3>{patientReport.clinic}</h3>
+        <p className="w-50 text-start">{patientReport.address}</p>
+      </div>
+      <div className="text-center">
+        <h1 className="text-center mb-5">Patient</h1>
+        {Object.keys(patientReport.patient).map((key) => (
+          <p>{patientReport.patient[key]}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PatientDetails;
---
+++ src/components/ManageAppointments/PatientReport.js
@@ -0,0 +1,20 @@
+module.exports.patientReport = {
+  clinic: "Toronto Public Clinic",
+  address: "123 Yonge St. Toronto, ON M3B 2T1",
+  patient: {
+    name: "Michael Scott",
+    address: "23 Dundas St. Toronto, ON M4S 2G5",
+    phone: "416-123-1234",
+    email: "Michael123@gmail.com",
+  },
+  reportDetails: {
+    created_on: "June 10th, 2016",
+    created_by: "Dr. Sam Henry",
+    last_updated: "June 12th, 2016",
+    last_updated_by: "Dr. Sam Henry",
+    information:
+      "Patient sprained their ankle while playing football. Told them they will need to get rest and ice their ankle. Also to take pain killers if needed. ",
+    medication_prescribed: "N/A",
+    referral: "N/A",
+  },
+};
