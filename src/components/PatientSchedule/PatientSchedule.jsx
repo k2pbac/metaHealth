5aa51c93faad068b20e954c9cd5a527378e6a1d7
k2pbac <krisbachan123@hotmail.com>
2021-10-27 20:57:35 -0400

    change appointment notes into a link to show a modal with details

--- src/components/PatientSchedule/PatientSchedule.jsx
+++ src/components/PatientSchedule/PatientSchedule.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import "./PatientSchedule.scss";
 
 const PatientSchedule = ({ patient }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Container
       className="justify-content-center align-items-center text-center d-flex flex-column"
@@ -18,7 +24,35 @@ const PatientSchedule = ({ patient }) => {
               <td>{appointment.date}</td>
               <td>{appointment.time}</td>
               <td>{appointment.clinic}</td>
-              <td>Appointment Notes</td>
+              <td>
+                <>
+                  <a
+                    onClick={handleShow}
+                    style={{
+                      cursor: "pointer",
+                      textDecoration: "underline",
+                      color: "blue",
+                    }}
+                  >
+                    Appointment Notes
+                  </a>
+
+                  <Modal animation={false} show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Appointment Notes</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{appointment.report}</Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
+                      </Button>
+                      <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </>
+              </td>
             </tr>
           ))}
         </tbody>
