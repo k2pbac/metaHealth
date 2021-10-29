bc363104327a1b6a40def6d8689750cee6990d84
k2pbac <krisbachan123@hotmail.com>
2021-10-29 12:52:20 -0400

    add state to login apge

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginForm.scss";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+
 const LoginForm = (props) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Container className="login-form w-50 h-100 text-center shadow">
       <h1 className="mb-4">Login</h1>
-      <Form className="w-50 mx-auto">
+      <Form className="w-50 mx-auto" onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="username">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" />
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
-        <div class="d-flex justify-content-center flex-column align-items-center">
+        <div className="d-flex justify-content-center flex-column align-items-center">
           <Button
             style={{ paddingLeft: "40px", paddingRight: "40px" }}
             variant="outline-success"
