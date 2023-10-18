50ff7df21e713ce7fd9f8bb0df9f31975e47b538
k2pbac <krisbachan123@hotmail.com>
2023-10-18 14:15:13 -0400

    update ssl

--- express-backend/db/index.js
+++ express-backend/db/index.js
@@ -1,10 +1,15 @@
 const pg = require("pg");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
-
+let connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+if ((process.env.NODE_ENV = "production")) {
+  connectionString = process.env.DATABASE_URL;
+}
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || connectionString,
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 client.connect();
