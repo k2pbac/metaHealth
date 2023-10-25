a4e8247dd80341bd0d784d5a25b90115c5898b1e
k2pbac <krisbachan123@hotmail.com>
2023-10-25 17:30:04 -0400

    undo comment of psql

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 let connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 if ((process.env.NODE_ENV = "production")) {
-  // connectionString = process.env.DATABASE_URL;
+  connectionString = process.env.DATABASE_URL;
 }
 const client = new pg.Client({
   connectionString: connectionString,
